30177bd45d35239c6767aee50b21434ce17ad109
Matthew Jones <Matthewkennethjones@gmail.com>
2023-05-02 20:06:04 -0600

    Group Project Rough Draft

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,13 +1,30 @@
-var product = {};
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
-export default async function productDeatils(productId) {
+let product = {};
 
+export default async function productDetails(productId) {
+    product = await findProductById(productId);
+    renderProductDetails();
+    document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 
 function addToCart() {
-
+    const cartItems = getLocalStorage("so-cart") || []; 
+    cartItems.push(product); 
+    setLocalStorage("so-cart", cartItems);
 }
 
 function renderProductDetails() {
-
+    document.querySelector("#productName").innerText = product.Brand.Name;
+    document.querySelector("#productNameWithoutBrand").innerText =
+      product.NameWithoutBrand;
+    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").alt = product.Name;
+    document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+    document.querySelector("#productColorName").innerText =
+      product.Colors[0].ColorName;
+    document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+      product.DescriptionHtmlSimple;
+    document.querySelector("#addToCart").dataset.id = product.Id;
 }
