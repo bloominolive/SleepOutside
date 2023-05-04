bfc3f0537eaf6ee74183643cafb44fe9ad784db4
bloominolive <bloomin.olive88@gmail.com>
2023-05-04 16:16:28 -0400

    Update to add shake and count to cart icon

--- src/js/product.js
+++ src/js/product.js
@@ -6,14 +6,53 @@ function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || []; 
   cartItems.push(product); 
   setLocalStorage("so-cart", cartItems); 
+  cartItemCountUpdate();
 }
-// add to cart button event handler
+
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
+  animateCart();
 }
 
-// add listener to Add to Cart button
+function animateCart(){
+  const cart = document.querySelector('.cart');
+  cart.classList.add('animate');
+  cart.addEventListener('animationend', () => {
+    cart.classList.remove('animate');
+  }, { once: true });
+}
+
+function cartItemCountUpdate(){
+  const count = getTotalItemsInCart();
+  const existingCircle = document.querySelector('.number-circle');
+  if (existingCircle){
+    const itemCount = document.querySelector('.item-count');
+    itemCount.innerText = count;
+  }
+  else{
+    const cart = document.querySelector('.cart');
+
+    const circleContainer = document.createElement('div');
+    circleContainer.classList.add('number-circle');
+
+    const circleNumber = document.createElement('span');
+    circleNumber.classList.add('item-count');
+    circleNumber.innerText = count;
+    circleContainer.appendChild(circleNumber);
+
+    cart.appendChild(circleContainer);
+  }
+  
+}
+
+function getTotalItemsInCart(){
+  const cartItems = getLocalStorage("so-cart");
+  const count = cartItems == null ? 0 : cartItems.length;
+  return count;
+}
+cartItemCountUpdate();
+
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
