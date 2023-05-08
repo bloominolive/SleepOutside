1a9ffcb80e6c15b14937574266ed2991ec864294
petosagie <petosagie@gmail.com>
2023-05-08 15:54:43 +0100

    Fixed with no items in the cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,10 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
+  if (cartItems.length === 0) {
+    return;
+  }
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
@@ -16,9 +19,6 @@ function cartItemTemplate(item) {
   </a>
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
